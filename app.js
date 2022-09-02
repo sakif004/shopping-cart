68fee7a4ae86315da89fc2c6a7291d820a1024c5
unknown <sakifur35-2749@diu.edu.bd>
2022-09-02 20:36:49 +0600

    update case price

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 function updateCaseNumber(isIncreasing) {
     const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
+    let caseNumber = caseInput.value;
     if (isIncreasing == true) {
-        caseInput.value = parseInt(caseNumber) + 1;
+        caseNumber = parseInt(caseNumber) + 1;
     }
     else if (caseNumber > 0) {
-        caseInput.value = parseInt(caseNumber) - 1;
+        caseNumber = parseInt(caseNumber) - 1;
     }
+    caseInput.value = caseNumber;
+    // update case total 
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
 };
 
 // increase case count 
