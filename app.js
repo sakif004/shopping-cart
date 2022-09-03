94ad05d8479ce424982f673e67164856341c0da1
unknown <sakifur35-2749@diu.edu.bd>
2022-09-03 09:55:51 +0600

    Calculate phone cost using the same shared function

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-function updateCaseNumber(isIncreasing) {
-    const caseInput = document.getElementById('case-number');
-    let caseNumber = caseInput.value;
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
     if (isIncreasing == true) {
-        caseNumber = parseInt(caseNumber) + 1;
+        productNumber = parseInt(productNumber) + 1;
     }
-    else if (caseNumber > 0) {
-        caseNumber = parseInt(caseNumber) - 1;
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
     }
-    caseInput.value = caseNumber;
-    // update case total 
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber * 59;
+    productInput.value = productNumber;
+    // update total 
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
 };
+// phone increase event
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1229, true);
+});
+// phone decrease event
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1229, false);
+});
 
 // increase case count 
 document.getElementById('case-plus').addEventListener('click', function () {
-    updateCaseNumber(true);
+    updateProductNumber('case', 59, true);
 });
 
 // decrease case count 
 document.getElementById('case-minus').addEventListener('click', function () {
-    updateCaseNumber(false);
+    updateProductNumber('case', 59, false);
 });
 
 
